990c94af44848fb1a2968e7d16e97b72cb30bc0c
Miranda Cooper <mleighc@umich.edu>
2022-04-16 22:20:53 -0400

    working on final project accessing data

--- Final_Proj_API.py
+++ Final_Proj_API.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import requests
 import csv
 import xmltodict, json
@@ -5,43 +6,66 @@ import xmltodict, json
 ####################################
 #READ IN A CSV WITH GAME ATTRIBUTES# from kaggle: https://www.kaggle.com/datasets/andrewmvd/board-games
 ####################################
-fields = []
-rows = []
 
-with open('bgg_dataset.csv','r') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=';')
+def read_csv(filename):
+    ''''DOCSTRING!'''
+    with open(filename,'r') as csvfile:
+        csv_reader = csv.reader(csvfile, delimiter=';')
+        line_count = 0
+        data = []
+        # print(type(csv_reader))
 
-    fields = next(csvreader)
-
-    for row in csvreader:
-        rows.append(row)
-
-    print(f"Total no. of rows: {csvreader.line_num}")
-
-for field in fields:
-    print(field)
-
-print(f'\nFirst 5 rows are:\n')
-for row in rows[:5]:
-    for col in row:
-        print(col,end=" ")
-    print('\n')
+        for row in csv_reader:
+            data.append(row)
+            return data
+            # if line_count == 0:
+        #         print(f'Column names are {", ".join(row)}')
+        #         line_count += 1
+        #     else:
+        #         print(", ".join(row))
+        #         line_count += 1
+    # print(f'Processed {line_count} lines.')
 
 
+def write_csv(filename, header, data):
+    '''DOCSTRING!'''
+    with open(filename, 'w', encoding='UTF8') as fileobj:
+        writer = csv.writer(fileobj)
+
+        writer.writerow(header)
+        writer.writerow(data)
 
 
 
 
-#####################################################
-#READ IN API REVIEW DATA FROM BOARDGAME GEEK XML API#: https://boardgamegeek.com/wiki/page/BGG_XML_API#
-#####################################################
+
+##############################################################
+#READ IN API DATA FROM BOARDGAME GEEK XML API (SET UP SEARCH)#: https://boardgamegeek.com/wiki/page/BGG_XML_API#
+##############################################################
 # #using the requests module to grab the xml data from the bgg api and then the xmltodict module to manipulate
 # #the xml as a json object
-# r = requests.get('https://boardgamegeek.com/xmlapi/boardgame/174430')
-# obj = xmltodict.parse(r.text)
-# # print(json.dumps(obj))
-# game = json.loads(json.dumps(obj))['boardgames']['boardgame'] #converting nested ordereddict to a dict: https://www.geeksforgeeks.org/how-to-convert-a-nested-ordereddict-to-dict/
-# print(type(game))
-# # print(game)
-# print(game.keys())
+r = requests.get('https://boardgamegeek.com/xmlapi/boardgame/174430')
+obj = xmltodict.parse(r.text)
+# print(json.dumps(obj))
+game = json.loads(json.dumps(obj))['boardgames']['boardgame'] #converting nested ordereddict to a dict: https://www.geeksforgeeks.org/how-to-convert-a-nested-ordereddict-to-dict/
+print(type(game))
 # print(game)
+print(game.keys())
+# print(game)
+
+
+
+def main():
+    '''DOCSTRING!'''
+    # data = read_csv('bgg_dataset.csv')
+    # print(data)
+    
+    # # filename = 'game_csv_test.csv'
+    # # header = fields
+    # # data = rows
+    # # write_csv(filename,header,rows)
+    pass
+
+
+if __name__ == '__main__':
+    main()
