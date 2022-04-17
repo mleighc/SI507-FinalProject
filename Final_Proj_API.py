10b4aacc5da2a49434ee6606a7f7976ffccec10d
Miranda Cooper <mleighc@umich.edu>
2022-04-16 21:38:32 -0400

    working on final project accessing data

--- Final_Proj_API.py
+++ Final_Proj_API.py
@@ -2,8 +2,9 @@ import requests
 import csv
 import xmltodict, json
 
-#READ IN A CSV WITH GAME ATTRIBUTES from kaggle: https://www.kaggle.com/datasets/andrewmvd/board-games
-######
+####################################
+#READ IN A CSV WITH GAME ATTRIBUTES# from kaggle: https://www.kaggle.com/datasets/andrewmvd/board-games
+####################################
 fields = []
 rows = []
 
@@ -27,14 +28,20 @@ for row in rows[:5]:
     print('\n')
 
 
-#READ IN API REVIEW DATA FROM BOARDGAME GEEK XML API: https://boardgamegeek.com/wiki/page/BGG_XML_API#
-######
-#using the requests module to grab the xml data from the bgg api and then the xmltodict module to manipulate
-#the xml as a json object
-r = requests.get('https://boardgamegeek.com/xmlapi/boardgame/174430')
-obj = xmltodict.parse(r.text)
-# print(json.dumps(obj))
-game = json.loads(json.dumps(obj))['boardgames']['boardgame'] #converting nested ordereddict to a dict: https://www.geeksforgeeks.org/how-to-convert-a-nested-ordereddict-to-dict/
-print(type(game))
+
+
+
+
+#####################################################
+#READ IN API REVIEW DATA FROM BOARDGAME GEEK XML API#: https://boardgamegeek.com/wiki/page/BGG_XML_API#
+#####################################################
+# #using the requests module to grab the xml data from the bgg api and then the xmltodict module to manipulate
+# #the xml as a json object
+# r = requests.get('https://boardgamegeek.com/xmlapi/boardgame/174430')
+# obj = xmltodict.parse(r.text)
+# # print(json.dumps(obj))
+# game = json.loads(json.dumps(obj))['boardgames']['boardgame'] #converting nested ordereddict to a dict: https://www.geeksforgeeks.org/how-to-convert-a-nested-ordereddict-to-dict/
+# print(type(game))
+# # print(game)
+# print(game.keys())
 # print(game)
-print(game.keys())
