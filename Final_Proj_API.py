b5b8e795fa1837fae48470870668cfb213254a51
Miranda Cooper <mleighc@umich.edu>
2022-04-20 17:11:19 -0400

    working on function code for setting up json objects to cache data

--- Final_Proj_API.py
+++ Final_Proj_API.py
@@ -1,3 +1,4 @@
+from unicodedata import category
 import requests
 import csv
 import xmltodict, json
@@ -6,38 +7,33 @@ import xmltodict, json
 #READ IN A CSV WITH GAME ATTRIBUTES# from kaggle: https://www.kaggle.com/datasets/andrewmvd/board-games
 ####################################
 
-def read_csv(filename):
-    ''''DOCSTRING!'''
-    with open(filename,'r') as csvfile:
-        csv_reader = csv.reader(csvfile, delimiter=';')
-        line_count = 0
+#a couple of helper functions below (write_json and write_dicts_to_csv) borrowed from SI506 Problem Sets written by Anthony Whyte
+def read_csv(filepath, encoding='utf-8', newline='', delimiter=','):
+    """
+    Reads a CSV file, parsing row values per the provided delimiter. Returns a list
+    of lists, wherein each nested list represents a single row from the input file.
+
+    Parameters:
+        filepath (str): The location of the file to read.
+        encoding (str): name of encoding used to decode the file.
+        newline (str): specifies replacement value for newline '\n'
+                       or '\r\n' (Windows) character sequences.
+        delimiter (str): delimiter that separates the row values
+
+    Returns:
+        list: a list of nested "row" lists
+    """
+
+    with open(filepath, 'r', encoding=encoding, newline=newline) as file_obj:
         data = []
-        # print(type(csv_reader))
-
-        for row in csv_reader:
+        reader = csv.reader(file_obj, delimiter=delimiter)
+        for row in reader:
             data.append(row)
-            return data
-            # if line_count == 0:
-        #         print(f'Column names are {", ".join(row)}')
-        #         line_count += 1
-        #     else:
-        #         print(", ".join(row))
-        #         line_count += 1
-    # print(f'Processed {line_count} lines.')
 
-######
-#Write a csv file
-def write_csv(filename, header, data):
-    '''DOCSTRING!'''
-    with open(filename, 'w', encoding='UTF8') as fileobj:
-        writer = csv.writer(fileobj)
-
-        writer.writerow(header)
-        writer.writerow(data)
+        return data
 
 ######
 #write a dictionary to json
-#helper functions below (write_json and write_dicts_to_csv) borrowed from SI506 Problem Sets written by Anthony Whyte
 def write_json(filepath, data, encoding='utf-8', ensure_ascii=False, indent=2):
     """Serializes object as JSON. Writes content to the provided filepath.
 
@@ -99,12 +95,49 @@ for item in hot_items:
     hot_item_data.append(d.copy()) #creating a list of dictionaries
     d.clear() #emptying the dict to start on the next dictionary to be appended
 print(len(hot_item_data))
-print(hot_item_data)
+# print(hot_item_data)
+
 ######
-# #write this to json file
+# NEXT: Enhance the data in this dictionary with additional features from BGG API, access individual HOT boardgames by their ID
 ######
-filepath = 'hot_boardgames.json'
-write_json(filepath,hot_item_data)
+#iterate over hot items list to grab the id and run an API request for each individual game within that list
+for item in hot_item_data:
+    id = item['id']
+    r = requests.get(f'https://boardgamegeek.com/xmlapi/boardgame/{id}')
+    obj = xmltodict.parse(r.text)
+    obj_details = json.loads(json.dumps(obj))['boardgames']['boardgame']
+
+
+    ####grab features to add to my dicts in hot_item_data
+    #description needs to be cleaned since it has line breaks indicated as <br>
+    descr = obj_details['description']
+    #image URL
+    image = obj_details['image']
+    #categories are nested in a list of dictionaries
+    categ = []
+    if isinstance(obj_details['boardgamecategory'],list):
+        for i in obj_details['boardgamecategory']:
+            categ.append(i['#text'])
+    else:
+        categ.append(obj_details['boardgamecategory']['#text'])
+
+
+    ###update the item's dictionary with these added features
+    item['description'] = descr
+    item['image'] = image
+    item['category_list'] = categ
+
+######
+#Read in bgg_dataset.csv to continue to enhance board game data
+filepath = 'bgg_dataset.csv'
+bgg = read_csv(filepath)
+print(len(bgg))
+
+######
+# #write hot_item_data to json file
+######
+# filepath = 'hot_boardgames.json'
+# write_json(filepath,hot_item_data)
 
 
 if __name__ == '__main__':
